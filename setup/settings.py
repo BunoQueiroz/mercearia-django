688d2bb2b2f10e1f84490796485a68251d95ae6e
Bruno Castro <bs8872491@gmail.com>
2022-10-30 14:10:33 -0300

    Configurando o banco de dados

--- setup/settings.py
+++ setup/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'merceariaDB',
+        'USER': 'postgres',
+        'PASSWORD': 'Bruno2022',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from produtos import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(urls))
+    path('', include('produtos.urls'))
 ]
