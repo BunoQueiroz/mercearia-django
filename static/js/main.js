86ac898ed557c8d693f35ef12d11e8e493d18388
Bruno Castro <bs8872491@gmail.com>
2022-10-30 09:48:31 -0300

    Separando a função fixar a barra de navegação em outro arquivo JavaScript

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,5 @@
+import { fixed } from "./navbarFixed.js";
+
 const navbar = document.querySelector('.navbar');
 
-window.addEventListener('scroll', () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
-    if(scrollTop > 120){
-        navbar.classList.add('fixed', 'py-0')
-    } else {
-        navbar.classList.remove('fixed', 'py-0')
-    }
-
-});
+fixed(navbar);
