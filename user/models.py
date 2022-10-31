7d69e97b7606d5046b33bd8dc5cd989c5d499c65
Bruno Castro <bs8872491@gmail.com>
2022-10-31 14:01:15 -0300

    Criando o modelo de usuários

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class People(models.Model):
+    username = models.CharField(max_length=50, blank=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=70)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
