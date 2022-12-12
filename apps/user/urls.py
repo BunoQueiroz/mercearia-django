d4404bec98d1ef9c41b0cedda238068aff2e106c
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-12 15:33:12 -0300

    Desenvolvendo a função para logar um usuário no sistema

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('login', login, name='login'),
     path('register', register, name='register'),
-    path('resgiter_user', register_user, name='register_user')
+    path('resgiter_user', register_user, name='register_user'),
+    path('login_user', login_user, name='login_user'),
 ]
--- apps/user/views/login.py
+++ apps/user/views/login.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def login(request):
     return render(request, 'user/login.html')
+
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user_data = User.objects
+
+        if user_data.filter(email=email).exists():
+            username = user_data.get(email=email)
+            user = auth.authenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request, user)
+                messages.success(request, 'Login realizado com sucesso')
+                return redirect('home')
+            messages.success(request, 'Algo deu errado')
+            return redirect('login')
+        messages.success(request, 'Email não cadastrado')
+        return redirect('login')
