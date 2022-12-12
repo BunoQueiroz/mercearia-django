31f6939d1f593ef42a1a31ba8f057cc8894a0469
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-12 15:32:19 -0300

    Adicionando as urls do app core e alterando o caminho de produtos

--- setup/urls.py
+++ setup/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')),
+    path('products/', include('products.urls')),
     path('user/', include('user.urls')),
+    path('', include('core.urls')),
 ]
