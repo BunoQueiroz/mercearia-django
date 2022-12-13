cce0e6d007dfcece7b7f56713782161d020d57b1
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-13 14:57:25 -0300

    Desenvolvendo o modelo de Produto e mandando a migração para o banco de dados

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100, blank=True)
+    date_create = models.DateTimeField(default=datetime.now())
+    image = models.ImageField(upload_to='products/images/%Y/%m/%d/')
+    price = models.FloatField()
+    category = models.CharField(max_length=20)
+    publicated = models.BooleanField(default=False)
