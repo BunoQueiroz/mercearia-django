c13f87ea00db84ae2d2ccd03f75f3f3a43cfdab6
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-14 15:40:39 -0300

    Permitindo que a flag de publicado seja alterado mais facilmente

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -3,7 +3,7 @@ from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'publicated', 'category')
-    list_editable = ('price',)
+    list_editable = ('price', 'publicated')
     list_display_links = ('category',)
     list_per_page = 10
     list_filter = ('name', 'category')
