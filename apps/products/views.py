321ecc014077b50e59fb2508191e69a0aa64bef6
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-14 15:39:29 -0300

    Realizando a pesquisa de somente produtos publicados. E caso não tenha produto, redirecionar para index

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from products.models import Product
 from django.contrib import messages
 
 def search(request):
     if 'search' in request.GET:
         search = request.GET.get('search').strip()
-        products = Product.objects.filter(name__icontains=search)
+        products = Product.objects.filter(name__icontains=search, publicated=True)
         if products.exists():
             context = {'products': products}
             return render(request, 'products/product_search.html', context)
         messages.error(request, 'Infelizmente ainda não trabalhamos com esse produto')
-        return render(request, 'products/product_search.html')
+        return redirect('home')
