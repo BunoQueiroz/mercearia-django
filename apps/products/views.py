6f8e8089867d3fc6d1fed40b7c36f5b4929bfa9a
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-14 11:03:53 -0300

    Desenvolvendo a função de pesquisa do app de produtos

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from products.models import Product
+from django.contrib import messages
 
-def index(request):
-    return render(request, 'products/index.html')
+def search(request):
+    if 'search' in request.GET:
+        search = request.GET.get('search').strip()
+        products = Product.objects.filter(name__icontains=search)
+        if products.exists():
+            context = {'products': products}
+            return render(request, 'products/product_search.html', context)
+        messages.error(request, 'Infelizmente ainda não trabalhamos com esse produto')
+        return render(request, 'products/product_search.html')
