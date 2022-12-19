a8dbcdece6e140f95653e56edaf60d122f243fb6
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-19 14:48:51 -0300

    Criando uma relação de uma para muitos entre conta e compra utilizando Foreign key

--- apps/user/models/account.py
+++ apps/user/models/account.py
@@ -1,10 +1,8 @@
 from django.db import models
 from .client import Client
-from .purchase import Purchase
 
 class Account(models.Model):
-    client = models.OneToOneField(Client, on_delete=models.PROTECT, related_name='Client repective')
-    purchase = models.ForeignKey(Purchase, on_delete=models.PROTECT, blank=True)
+    client = models.OneToOneField(Client, on_delete=models.PROTECT, related_name='Client')
 
     def __str__(self) -> str:
-        return self.client
+        return self.client.first_name
--- apps/user/models/purchase.py
+++ apps/user/models/purchase.py
@@ -1,12 +1,14 @@
 from django.db import models
 from datetime import datetime
 from products.models import Product
+from .account import Account
 
 class Purchase(models.Model):
-    hour = models.DateTimeField(default=datetime.now())
+    hour = models.DateTimeField(default=datetime.today())
     items = models.ForeignKey(Product, on_delete=models.PROTECT)
-    amount = models.IntegerField()
+    amount = models.IntegerField(default=1)
+    account = models.ForeignKey(Account, on_delete=models.PROTECT)
 
     def __str__(self) -> str:
-        hour_and_items = f'{self.hour} - {self.items}'
-        return hour_and_items
+        items_and_amount = f'{self.items} - {self.amount}'
+        return items_and_amount
