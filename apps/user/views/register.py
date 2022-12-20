e23ceb29ba2f59a337e28a44b9f5a85325a803bb
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-20 14:36:44 -0300

    Otimizando o cadastro de usuários

--- apps/user/views/register.py
+++ apps/user/views/register.py
@@ -15,13 +15,11 @@ def register_user(request):
         confirm_password = request.POST['confirmPassword']
 
         if(password != confirm_password or len(str(password)) < 8 or User.objects.filter(email=email).exists()):
-            current_page = 'register'
             messages.error(request, 'Por favor, verifique sua senha, ou insira um email válido')
-            
-            return redirect(current_page)
+            return redirect('register')
 
-        else:
-            Client.objects.create_user(username=email, first_name=first_name, last_name=last_name, email=email, password=password, is_superuser=False)
-            messages.success(request, 'Cadastro realizado com sucesso')
-
-            return redirect('login')
+        Client.objects.create_user(username=email, first_name=first_name, last_name=last_name, email=email, password=password, is_superuser=False)
+        messages.success(request, 'Cadastro realizado com sucesso')
+        return redirect('login')
+        
+    return redirect('register')
