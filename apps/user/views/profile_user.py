ae50a95eea7e783850be8c5e753bb0b4bb250bef
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-21 11:23:55 -0300

    Desenvolvendo a função de atualizar perfil dos clientes

--- apps/user/views/profile_user.py
+++ apps/user/views/profile_user.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render, redirect
+from user.models import Client
+from django.contrib import messages
 
 def profile(request):
     if request.user.is_authenticated:
-        return render(request, 'user/profile.html')
+        client = get_client(request)
+        context = {'client': client}
+        return render(request, 'user/profile.html', context)
     return redirect('home')
 
 def update_profile(request):
-    return render(request, 'user/profile.html')
+    if request.method == 'POST' and request.user.is_authenticated:
+        client = get_client(request)
+        if 'img' in request.FILES:
+            img = request.FILES['img']
+            client.image = img # type: ignore
+
+        client.first_name = request.POST.get('firstName')
+        client.last_name = request.POST.get('lastName')
+        client.email = request.POST.get('email')
+        client.username = request.POST.get('username')
+
+        try:
+            client.save()
+        except:
+            messages.error(request, 'Usuário ou Email Inválido')
+            return redirect('profile')
+            
+        messages.success(request, 'Dados alterados com sucesso')
+        return redirect('profile')
+
+    return redirect('profile')
+
+def get_client(request):
+    possible_client = request.user.username
+    client = Client.objects.filter(username=possible_client).get()
+    return client
