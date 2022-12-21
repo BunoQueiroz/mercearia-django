b32c40dd6e6809bd78f6fabf734b3d1e873b57b9
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-21 15:33:25 -0300

    Redirecionando os usuários autenticados para a dashboard

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
     return render(request, 'core/index.html')
