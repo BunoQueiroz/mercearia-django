02f5d01f4a9ed21858f0ec20b5555309a48954d4
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-21 15:00:15 -0300

    Criando a classe de categoria e a usando, como chave estrangeira, na classe de produto

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from datetime import datetime
 
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=100, blank=True)
     date_create = models.DateTimeField(default=datetime.now())
     image = models.ImageField(upload_to='products/images/%Y/%m/%d/')
     price = models.FloatField()
-    category = models.CharField(max_length=20)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     publicated = models.BooleanField(default=False)
 
     def __str__(self) -> str:
