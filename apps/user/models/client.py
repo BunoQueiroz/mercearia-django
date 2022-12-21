aa27e10944663dc379ba65e1611bc874db39748d
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-21 09:58:15 -0300

    Alterando caminho dos arquivos de imagem dos clientes

--- apps/user/models/client.py
+++ apps/user/models/client.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Client(User):
-    image = models.ImageField(upload_to='%Y/%m/%d/', blank=True)
+    image = models.ImageField(upload_to='clients/%Y/%m/%d/', blank=True)
 
     def __str__(self) -> str:
         return self.first_name
