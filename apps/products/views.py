e90128fae394d4c29524ef9bfb9c057f2e28905e
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-21 15:01:21 -0300

    Desenvolvendo uma função para mostrar todos os produtos publicados

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, redirect
-from products.models import Product
+from products.models import Product, Category
 from django.contrib import messages
+
+def all_products(request):
+    products = Product.objects.filter(publicated=True)
+    categorys = Category.objects.all()
+    context = {
+        'products': products,
+        'categorys': categorys
+    }
+    for product in products:
+        print(product.category)
+    return render(request, 'products/all_products.html', context)
 
 def search(request):
     if 'search' in request.GET:
@@ -11,3 +22,4 @@ def search(request):
             return render(request, 'products/product_search.html', context)
         messages.error(request, 'Infelizmente ainda não trabalhamos com esse produto')
         return redirect('home')
+    return redirect('home')
