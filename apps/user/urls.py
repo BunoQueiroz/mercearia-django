7a8768ee768bab35bf0729113d0071a11e4115e5
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-26 10:10:08 -0300

    Adicionando o caminho da url para alterar senha dos clientes

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('my_profile', profile, name='profile'),
     path('update_profile', update_profile, name='update_profile'),
     path('my_account', my_account, name='my_account'),
+    path('set_password', set_password_client, name='set_password'),
 ]
