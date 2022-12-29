ae30bc990a53a454b16edc3e63bda3ee6691950b
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-29 15:21:40 -0300

    Deixando disponível no módulo de views do app de user somente as principais funções

--- apps/user/views/__init__.py
+++ apps/user/views/__init__.py
@@ -1,7 +1,7 @@
-from .login import *
-from .register import *
-from .logout import *
-from .dashboard import *
-from .profile_user import *
+from .login import login, login_client
+from .register import register, register_client
+from .logout import logout
+from .dashboard import dashboard
+from .profile_user import profile, update_profile
 from .account import my_account
 from .change_password import set_password_client
