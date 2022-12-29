55ee2428946998d071ab499506837401bb14d373
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2022-12-29 15:23:48 -0300

    Alterando o nome de algumas urls do app user (user) -> (client)

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     path('login', login, name='login'),
     path('register', register, name='register'),
-    path('resgiter_user', register_user, name='register_user'),
-    path('login_user', login_user, name='login_user'),
+    path('resgiter_client', register_client, name='register_user'),
+    path('login_client', login_client, name='login_user'),
     path('logout', logout, name='logout'),
     path('dashboard', dashboard, name='dashboard'),
     path('my_profile', profile, name='profile'),
