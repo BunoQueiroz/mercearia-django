a852751d809f789e4a931054ccb7644b9730ff9d
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-11 14:20:12 -0300

    Adicionando o caminho de envio de mensagem dos usuários do sistema

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('send_message', views.send_by_email, name='send_message'),
 ]
