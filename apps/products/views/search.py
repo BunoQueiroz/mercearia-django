1e11cde66d61ba4ddb4861c840a443d716fc4095
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-12 14:13:46 -0300

    Subdividindo a função de pesquisa em outras menores

--- apps/products/views/search.py
+++ apps/products/views/search.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, redirect
 from products.models import Product
-from django.contrib import messages
+from core.views.utils import get_search_field_serialized, message_error_and_redirect
 
 def search(request):
     if 'search' in request.GET:
-        search = request.GET.get('search').strip()
-        products = Product.objects.filter(name__icontains=search, publicated=True)
-        if products.exists():
-            context = {'products': products}
-            return render(request, 'products/product_search.html', context)
-        messages.error(request, 'Infelizmente ainda não trabalhamos com esse produto')
-        return redirect('home')
+        return result_or_none(request)
     return redirect('home')
+
+def result_or_none(request):
+    search = get_search_field_serialized(request, 'search')
+    products = Product.objects.filter(name__icontains=search, publicated=True)
+    return render_with_filter(request, products, 'products')
+
+def render_with_filter(request, search, name_in_template):
+    if search.exists():
+        context = {f'{name_in_template}': search}
+        return render(request, 'products/product_search.html', context)
+    return message_error_and_redirect(request, 'Infelizmente ainda não trabalhamos com esse produto', 'dashboard')
