5f073b6ffead15c098266cccc70c8bc5642de9b3
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-15 15:43:14 -0300

    Criando uma função para renderizar o formulário de contato

--- apps/core/views/home.py
+++ apps/core/views/home.py
@@ -1,6 +1,14 @@
 from  django.shortcuts import redirect, render
+from core.forms import ContactForms
 
 def home(request):
     if request.user.is_authenticated:
         return redirect('dashboard')
-    return render(request, 'core/index.html')
+    return render(request, 'core/index.html', context=render_form_contact())
+
+def render_form_contact():
+    contact = ContactForms()
+    context = {
+        'form': contact
+    }
+    return context
