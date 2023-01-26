e7250a21ce80299a4aa5a9bec9f9c90bf1a9c64f
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-26 08:22:00 -0300

    Desenvolvendo um modelo para contato

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=99999999999)
+    message = models.CharField(max_length=256)
+
+    def __str__(self) -> str:
+        return self.email
