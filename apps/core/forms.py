2b9fcd7d790a59e380f180628c7e476e7e7a76ce
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-26 08:28:16 -0300

    Desenvolvendo um formulário de contato baseado em um modelo já existente e validando-o

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -1,12 +1,33 @@
 from django import forms
+from core.validators import *
+from core.models import Contact
 
-class ContactForms(forms.Form):
-    full_name = forms.CharField(max_length=100, label='Nome completo', required=True)
-    email = forms.EmailField(max_length=100, label='Email', required=True)
-    phone = forms.IntegerField(max_value=99999999999, min_value=11911111111, required=False)
-    message = forms.CharField(
-        max_length=256,
-        widget=forms.Textarea(),
-        label='Mensagem',
-        required=True
-    )
+class ContactForms(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+        widgets = {
+            'message': forms.Textarea(),
+        }
+        labels = {
+            'full_name': 'Nome completo',
+            'email': 'Email',
+            'phone': 'Telefone',
+            'message': 'Mensagem',
+        }
+
+    def clean(self):
+        full_name = self.cleaned_data.get('full_name')
+        phone = self.cleaned_data.get('phone')
+        message = self.cleaned_data.get('message')
+        errors_list = {}
+        
+        non_numeric_field(full_name, 'full_name', errors_list)
+        phone_valid(phone, 'phone', errors_list)
+        message_valid(message, 'message', errors_list)
+
+        if errors_list is not None:
+            for error in errors_list:
+                message_error = errors_list[error]
+                self.add_error(error, message_error)
+        return self.cleaned_data
