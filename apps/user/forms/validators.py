142d0360d1ce067c2ca94cf937f26882fca97ee1
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-27 10:05:19 -0300

    Desenvolvendo validador de login

--- apps/user/forms/validators.py
+++ apps/user/forms/validators.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import User
+from user.models import Client
+from django.contrib import auth
 
 def password_validator(password, confirm_password, errors_list):
     password_short(password, errors_list)
@@ -13,7 +14,18 @@ def different_passwords(password, confirm_password, erros_list):
         erros_list['confirm_password'] = 'Suas senhas estão diferentes'
 
 def repeated_email(email, errors_list):
-    mail = User.objects.filter(email=email)
+    mail = Client.objects.filter(email=email)
     if mail.exists():
         errors_list['email'] = 'Email já Cadastrado'
+
+def login_validator(email, password, errors_list):
+    mail = Client.objects.filter(email=email)
+    if not mail.exists():
+        errors_list['email'] = 'Email não Cadastrado'
+        return
+
+    user = Client.objects.get(email=email)
+    client = auth.authenticate(username=user.username, password=password)
+    if client is None:
+        errors_list['password'] = 'Credenciais não reconhecidas'
 
