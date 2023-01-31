99e445a1864056177828f84899e1efffb696a890
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-31 14:22:25 -0300

    Alterando o model admin de produtos

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -4,7 +4,7 @@ from .models import *
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'publicated', 'category')
     list_editable = ('price', 'publicated')
-    list_display_links = ('category',)
+    list_display_links = ('category', 'name')
     list_per_page = 10
     list_filter = ('name', 'category')
 
