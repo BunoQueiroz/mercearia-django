df58a09423dd58ce8ab5c7c614aeba00badaf93f
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-31 10:03:35 -0300

    Definindo uma url para a página de contato

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,5 +3,6 @@ from core.views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('contact', contact, name='contact'),
     path('send_message', send_by_email, name='send_message'),
 ]
