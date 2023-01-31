4e9b70fd899d582cb6a557b248e66595ea976a46
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-31 10:05:46 -0300

    Removendo a função para renderizar o formulário de contato da View Home

--- apps/core/views/home.py
+++ apps/core/views/home.py
@@ -1,14 +1,6 @@
 from  django.shortcuts import redirect, render
-from core.forms import ContactForms
 
 def home(request):
     if request.user.is_authenticated:
         return redirect('dashboard')
-    return render(request, 'core/index.html', context=render_form_contact())
-
-def render_form_contact():
-    contact = ContactForms()
-    context = {
-        'form': contact
-    }
-    return context
+    return render(request, 'core/index.html')
