1e67e9e2edc5fa4dba3fd11a31f5ed3b63fb9fe2
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-31 15:31:13 -0300

    Adicionando uma url para as landing pages dos produtos

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', all_products, name='all_products'),
     path('search', search, name='search'),
+    path('landing_page/<int:product_id>', landing_page, name='landing_page'),
 ]
