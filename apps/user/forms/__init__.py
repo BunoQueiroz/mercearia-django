785cd3ed7b856c2c517e3508eab27ac80508d4b5
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-02-02 14:14:15 -0300

    Importando o modelo de alterar senhas para o inicializador do módulo de formulário no app de user

--- apps/user/forms/__init__.py
+++ apps/user/forms/__init__.py
@@ -2,3 +2,4 @@ from .register import ClientRegisterForm
 from .validators import password_validator
 from .login import LoginForm
 from .update_client import UpdateClientForm
+from .change_password import ChangePasswordForm
