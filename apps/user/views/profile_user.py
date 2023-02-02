94b584272a897072467bc89495bd6b813fd4fd29
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-02-02 14:19:22 -0300

    Refatorando a view de profile_user para passar uma instância do formulário para alterar senha

--- apps/user/views/profile_user.py
+++ apps/user/views/profile_user.py
@@ -1,21 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from user.models import Client
-from core.views.utils import message_success_and_redirect, get_field_serialized
-from user.forms import UpdateClientForm
+from core.views.utils import message_success_and_redirect, get_field_serialized, get_object_client
+from user.forms import UpdateClientForm, ChangePasswordForm
 
 def profile(request):
     if request.user.is_authenticated:
-        username = request.user.username
-        user = get_object_or_404(Client, username=username)
-        form = UpdateClientForm(instance=user)
-        context = {'form': form}
+        context = create_context_profile(request)
         return render(request, 'user/profile.html', context)
     return redirect('home')
 
 def update_profile(request):
     form = UpdateClientForm(request.POST)
     if request.method == 'POST' and form.is_valid():
-        client = get_client(request)
+        client = get_object_client(request)
         set_all_fields_client(request, client)    
         return save_client(request, client)
     return render(request, 'user/profil.html', {'form': form})
@@ -27,10 +24,16 @@ def set_image_client(request, client):
     if request.POST.get('image-clear') == 'on':
         client.image = ''
 
-def get_client(request):
-    possible_client = request.user.username
-    client = Client.objects.filter(username=possible_client).get()
-    return client
+def create_context_profile(request, data=None, *args, **kwargs):
+    username = request.user.username
+    user = get_object_or_404(Client, username=username)
+    form = UpdateClientForm(instance=user)
+    change_password = ChangePasswordForm(data, initial={'email': user.email})
+    context = {
+        'form': form,
+        'change_password': change_password,
+    }
+    return context
 
 def save_client(request, client):
     client.save()
