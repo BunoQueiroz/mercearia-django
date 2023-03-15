167275b695f6fe82db4b03e16a8462e2f90cbfd8
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-15 10:34:55 -0300

    Otimizando a view sw landing page

--- apps/products/views/landing_page.py
+++ apps/products/views/landing_page.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_list_or_404
 from products.models import Product
 
 def landing_page(request, product_id):
-    product = Product.objects.filter(pk=product_id)
-    if product.exists():
-        product = get_object_or_404(Product, pk=product_id)
-        return render(request, 'products/landing_page.html', {'product': product})
-    return  redirect('all_products')
+    product = get_list_or_404(Product, pk=product_id, publicated=True)[0]
+    return render(request, 'products/landing_page.html', {'product': product})
