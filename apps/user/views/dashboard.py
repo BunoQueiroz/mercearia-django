3169ba1a7ba74fcb43da158a0a887039391722b1
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-09-06 11:22:51 -0300

    Renderizando o template index quando não houver produtos cadastrados

--- apps/user/views/dashboard.py
+++ apps/user/views/dashboard.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from user.models import Product
-from core.views.utils import message_info_and_redirect
+from core.views.utils import message_info_and_render
 
 def dashboard(request):
     if request.user.is_authenticated:
@@ -11,7 +11,11 @@ def dashboard(request):
 def show_products_or_404(request, products):
     if products.exists():
         return show_products(request, products)
-    return message_info_and_redirect(request, 'Infelizemente ainda não temos produtos disponíveis', 'dashboard')
+    return message_info_and_render(
+        request,
+        'Infelizemente ainda não temos produtos disponíveis',
+        'core/index.html'
+        )
 
 def show_products(request, products):
     context = {'products': products}
