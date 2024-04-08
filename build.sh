74095f6b55d0fef82a872e91db5f691e2c08b480
Bruno de Castro <103578437+BunoQueiroz@users.noreply.github.com>
2024-04-08 15:25:03 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pip install --upgrade pip
+# Instalar as dependências Python
+pip install -r requirements.txt
+
+# Executar as migrações do banco de dados, se necessário
+python manage.py migrate
+
+# Outras tarefas de construção, como compilar assets estáticos
+python manage.py collectstatic --no-input
