9bde01b8e9a5cec71f7badc82bc3d7bbc150732e
Bruno de Castro <103578437+BunoQueiroz@users.noreply.github.com>
2024-08-26 15:57:12 -0300

    Ajuste de versão python no build

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Coleta de arquivos estáticos
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Criação de um arquivo de servidor para o Serverless Functions
 echo 'from django.core.wsgi import get_wsgi_application
