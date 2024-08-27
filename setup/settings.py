10d3e5beabcdff4b7111d2c299c1f5bb9e2ef116
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2024-08-27 13:34:25 -0300

    Configurações extras do whitenoise

--- setup/settings.py
+++ setup/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [
     os.path.join('apps/products/static'),
     os.path.join('apps/user/static'),
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
