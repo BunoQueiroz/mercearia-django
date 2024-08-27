a5ef2dd2affed789c81862439c62bc2e521dc00a
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2024-08-27 15:33:50 -0300

    Removendo criação de server.py em produção através do arquivo build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-# Coleta de arquivos estáticos
 python3 manage.py collectstatic --noinput
-
-# Criação de um arquivo de servidor para o Serverless Functions
-echo 'from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()' > server.py
