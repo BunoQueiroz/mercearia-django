061ac723842a17f9720c3ccbe46deb0118a62f48
Bruno de Castro <103578437+BunoQueiroz@users.noreply.github.com>
2024-10-21 13:31:33 -0300

    Update urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -4,10 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('user/', include('user.urls')),
-    path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'core.views.handler404'
